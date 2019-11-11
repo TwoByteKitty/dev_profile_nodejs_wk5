1d38182c80d09499f4618d70f28d110b2fb85212
Christie Burt <cmburt@yahoo.com>
2019-11-10 19:05:28 -0500

    made the md template literal; got name color changing based on user input; fixed html default ugly colors with horrible names

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const inqQuestions = [
         message: "What is your favorite color?",
         name: "favColor",
         choices: [
-            "vermillion",
+            "sanguine",
             "tangerine",
             "blonde",
-            "emerald",
+            "vert",
             "azure",
             "cobalt",
             "violaceous"
@@ -24,29 +24,75 @@ const inqQuestions = [
     }
 ];
 
-function createMarkdown({data:githubProfileData}=response
-    ){
-    console.log(githubProfileData);
-    function returnMD(err){
-        if (err){
-            return console.log (err);
+function betterColors(colorName) {
+    switch (colorName) {
+        case "sanguine":
+            return "#cc0000";
+            break;
+        case "tangerine":
+            return "#ff471a";
+            break;
+        case "blonde":
+            return "#ffb84d";
+            break;
+        case "vert":
+            return "#166941";
+            break;
+        case "azure":
+            return "#0086b3";
+            break;
+        case "cobalt":
+            return "#003cb3";
+            break;
+        case "violaceous":
+            return "#8000ff";
+            break;
+        default:
+            return "black";
+    }
+
+}
+
+function createMarkdown(githubProfileData) {
+    function returnMD(err) {
+        if (err) {
+            return console.log(err);
         }
         console.log("success!");
     }
-    function formatMD(){
-        
+    function formatMD() {
+        return `#<span style="color:${betterColors(githubProfileData.color)};">${githubProfileData.name}</span> 
+---
+>
+>![picture](${githubProfileData.avatar_url})
+>
+>**Bio:** ${githubProfileData.bio}
+>
+>**Location:** ${githubProfileData.location}
+>
+>**Company:** ${githubProfileData.company}
+>
+>**Public Repositories:** ${githubProfileData.public_repos}
+>
+>**Followers:** ${githubProfileData.followers}
+>
+>**Following:** ${githubProfileData.following}
+>
+`;
     }
-    fs.writeFile("profile.md", githubProfileData, returnMD)
+    fs.writeFile("profile.md", formatMD(), returnMD)
 }
 
-function handleInput(userInput){
+function handleInput(userInput) {
 
     const githubProfileUrl = `https://api.github.com/users/${userInput.userName}`;
-    
+
     console.log(githubProfileUrl);
 
     axios.get(githubProfileUrl)
-    .then(createMarkdown)
+        .then(({ data }) => {
+            createMarkdown({ color: userInput.favColor, ...data })
+        })
 }
 
 inquirer.prompt(inqQuestions).then(handleInput)
--- profile.md
+++ profile.md
@@ -1,1 +1,17 @@
-[object Object]
+#<span style="color:#166941;">Allison Payne</span> 
+---
+>
+>![picture](https://avatars1.githubusercontent.com/u/5451247?v=4)
+>
+>**Bio:** I am an avid gamer and disc golfer who moonlights as a Software Engineer, specializing in front end web technologies.
+>
+>**Location:** Charlotte, NC
+>
+>**Company:** null
+>
+>**Public Repositories:** 5
+>
+>**Followers:** 3
+>
+>**Following:** 1
+>
